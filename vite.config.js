8debc1e56c1a422ad472149223733195ca0ef92b
TCHINDAJOEL <tchindajoel25120@gmail.com>
2025-11-25 12:09:23 +0100

    chore: Update Vite dependency and remove trailing slash from base path configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nafankap_test1/',   // ← OBLIGATOIRE POUR GITHUB PAGES
+  base: '/nafankap_test1',   // ← OBLIGATOIRE POUR GITHUB PAGES
 })
